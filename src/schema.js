3341d5fbf81d3ec0106489a5d9da736567220d2c
spedfire1231 <pablosidels@gmail.com>
2021-09-13 00:42:14 +0300

    fix

--- src/schema.js
+++ src/schema.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongo = require('mongoose')
 
 module.exports = mongo.model(
@@ -25,32 +24,4 @@ module.exports = mongo.model(
         userId: String,
         bankcoins: Number,
     })
-=======
-const mongo = require('mongoose')
-
-module.exports = mongo.model(
-    'Money',
-    new mongo.Schema({
-        id: String,
-        userId: String,
-        userBit: String,
-        userVip: String,
-        userName: String,
-        VIP: Number,
-        coins: Number,
-        bankcoins: Number,
-        bitcoins: Number,
-        job: String,
-        name: String,
-    })
-)
-
-
-/*module.exports = mongo.model(
-    'Banks',
-    new mongo.Schema({
-        userId: String,
-        bankcoins: Number,
-    })
->>>>>>> 2b8e3f798a5b038a5b162c1bb2c6a5be5a0f6433
 )*/
